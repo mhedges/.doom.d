a75f31bcfd4d6c488363afebfaa8e87a88a09434
Michael Hedges <hedges7@purdue.edu>
2019-03-07 12:33:56 -0600

    First commit of DOOM configs

---
+++ packages.el
@@ -0,0 +1,10 @@
+;; -*- no-byte-compile: t; -*-
+;;; ~/.doom.d/packages.el
+
+;;; Examples:
+;; (package! some-package)
+;; (package! another-package :recipe (:fetcher github :repo "username/repo"))
+;; (package! builtin-package :disable t)
+(package! ox-html)
+(package! ox-latex)
+(package! ox-beamer :recipe ())
